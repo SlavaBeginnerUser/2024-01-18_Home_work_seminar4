54499f56c6ecca141daae95249cd6e3defdeb4a2
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-14 11:33:28 +0300

    add lesson3 task2

---
+++ Lesson03/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// вывод квадрата числа от 1 до N (N = 5, 10, 15) 
+void PrintSquares(int limit)
+{
+	int i = 1;
+	while (i<= limit)
+	{
+		Console.Write($"{i*i} ");
+		i++;
+	}
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
