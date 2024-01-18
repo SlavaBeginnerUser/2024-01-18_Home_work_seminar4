7c37e5ce4df5cc14f589fb319361981bc1883933
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-18 09:31:00 +0300

    add Home_Work Task1

---
+++ Seminar4/Home_Work/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная.
+
+// определяем количество разрядов digitNumber в введенном числе inputNumber
+// Console.WriteLine("Введите целое число: ");
+// int inputNumber = Convert.ToInt32(Console.ReadLine());
+// int digitNumber = 0;
+// while (inputNumber != 0)
+// {
+// 	inputNumber = inputNumber / 10;
+// 	digitNumber++;
+// }
+// Console.Write($"Количество разрядов равно {digitNumber} ");
+
+
+
+int sumOfDigits = 0;
+while (true) // бесконечный запуск программы до команды break
+{
+Console.WriteLine("Введите целое число: ");
+string inputstring= Convert.ToString(Console.ReadLine()); // преобразование в "строку"
+if(inputstring == "q")
+	break;
+int inputNumber = Convert.ToInt32(inputstring); // преобразование в число
+sumOfDigits = 0;
+while (inputNumber != 0)
+{
+	sumOfDigits = sumOfDigits + inputNumber % 10;;
+	inputNumber = inputNumber / 10;
+}
+if(sumOfDigits % 2 == 0)
+{
+		Console.Write($"Сумма цифр четная и равна {sumOfDigits} "); 
+		break;
+}
+}
