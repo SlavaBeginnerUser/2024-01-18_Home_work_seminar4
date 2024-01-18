db0c9d3b6972e6638ec6f362b4c1346f093765f8
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-18 11:13:48 +0300

    Home Work complete

--- Seminar4/Home_Work/Program.cs
+++ Seminar4/Home_Work/Program.cs
@@ -1,17 +1,6 @@
 ﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная.
 
-// определяем количество разрядов digitNumber в введенном числе inputNumber
-// Console.WriteLine("Введите целое число: ");
-// int inputNumber = Convert.ToInt32(Console.ReadLine());
-// int digitNumber = 0;
-// while (inputNumber != 0)
-// {
-// 	inputNumber = inputNumber / 10;
-// 	digitNumber++;
-// }
-// Console.Write($"Количество разрядов равно {digitNumber} ");
-
 
 // // Задача1 - решение
 // int sumOfDigits = 0;
