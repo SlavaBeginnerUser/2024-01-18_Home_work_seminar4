6c746e3b3723d5ad69c97d0d010dcd3bfedac012
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-19 16:33:21 +0300

    add Task3 to Lesson4

---
+++ Lesson04/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача
+// Считать с консоли строку, состоящую из цифр и латинских букв.
+// Сформировать новую строку, состоящую из букв исходной строки.
+
+
+string GetLettersFromString(string s)
+{
+	string letters = "";     // Ввод строки 
+	foreach (char e in s)    // Проход по элементам строки
+	{
+		if (char.IsAsciiLetter(e) == true)   // является ли проверяемый элемент буквой
+		{
+			letters = letters + e;   // дописываем подходящий элемент к новой строке 
+		}
+	}
+	return letters;
+}
+
+string str = Console.ReadLine();    // ввод строки
+string result = GetLettersFromString(str);  // вызов функции GetLettersFromString
+Console.WriteLine(result);          
