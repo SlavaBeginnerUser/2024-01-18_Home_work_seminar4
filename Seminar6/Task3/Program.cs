fef6aeab52b5f4d10acd028bb7b2e9bd2ef2262f
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-23 10:42:29 +0300

    add Task3 to Seminar6

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// Выяснить, сколько среди введённых букв гласных. 
+// Пример 	“hello” => 2 		“world” => 1
+
+// string hel ="HELLO!";
+// hel.ToLower(); // преобразование верхн региста символов в нижний
+
+int GetVowelsFromString(string str)
+{
+	int count = 0;
+	string vowels = "aeiouy"; // переменная vowels содержит все англ гласные в нижнем регистре
+	foreach (char e in str)  // проверка на наличие совпадений
+	{
+		if (vowels.Contains(e))
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+Console.Write("Введите строку: ");
+string str = Console.ReadLine();
+int res = GetVowelsFromString(str.ToLower());  // применяем метод, сразу переводим символы в нижний регистр
+Console.WriteLine($"Количество глассных в строке равно: {res}");
