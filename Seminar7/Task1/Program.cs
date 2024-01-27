3ac385725bc8092623c3fd5507d3c254ceb53aed
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-27 12:12:13 +0300

    add Task1 to Seminar7

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. 
+// Указание Использовать рекурсию. 
+// Пример 
+// 123 => 6 
+// 63 => 9
+
+int SumOfDigits(int num)
+{
+	if (num == 0) // условие выхода из рекурсии
+	{
+		return 0;
+	}
+	int result = num % 10 + SumOfDigits(num / 10);
+	return result;
+}
+
+Console.WriteLine(SumOfDigits(1234));
