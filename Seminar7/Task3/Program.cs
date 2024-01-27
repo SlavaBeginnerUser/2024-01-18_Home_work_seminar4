54791b239d0357cefe55813d2a5a9955e9e6fc67
Viacheslav Avdeev <slava708@yandex.ru>
2024-01-27 14:56:07 +0300

    add Task3 to Seminar7

---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. 
+// Вывести на экран согласные буквы этой строки. 
+// Указание Использовать рекурсию. Не использовать цикл. 
+// Пример 
+// “hello” => h l l 
+// “World” => W r l d 
+// “Hello world!” => H l l w r l d
+
+using System.ComponentModel;
+
+void ShowLetters(string str)
+ {
+	if(str.Length == 0)
+	{
+		return ;
+	}
+	string vowels = "aouyie";
+	if(!vowels.Contains(Char.ToLower((str[0]))) && char.IsAsciiLetter(str[0])) //если не содержит voweels переводим 1й элем строки в char и в нижн регистр => то 
+	{
+		Console.Write($"{str[0]} ");
+	}
+	ShowLetters(str.Substring(1));	// удаляет (1)-один символ в строке
+ }
+
+ShowLetters("Hello World!");
